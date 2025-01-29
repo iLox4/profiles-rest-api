5ee2cd7dfc5dc0b625bcd6e7fe9d684c21a66946
iLox4 <ilyaumnovvv@gmail.com>
2025-01-29 20:55:16 +0100

    Add post method to the api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import HelloSerializer
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -14,3 +19,14 @@ class HelloApiView(APIView):
         ]
 
         return Response({ 'message': 'Hello!', 'features': features })
+    
+    def post(self, request):
+        """Create a hello message with name from the request"""
+        serializer = self.serializer_class(data=request.data)
+
+        # Is valid if request contains required fields and additional fields
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        name = serializer.validated_data.get('name')
+        return Response({ 'message': f'Hello {name}'})
